d2650ab8ca334026e0db79250b97f3b7cb030912
Riley <77288737+riley0122@users.noreply.github.com>
2024-10-03 22:02:11 +0200

    Changed string

--- projectSource/src/main.cpp
+++ projectSource/src/main.cpp
@@ -6,7 +6,8 @@
 int main(int argc, char *argv[]) {
     EADK::Display::pushRectUniform(EADK::Screen::Rect, Indigo);
 
-    EADK::Display::drawString("Started succesfully!", EADK::Point(0,0), false, White, Indigo);
+    EADK::Display::drawString("Started succesfully!", EADK::Point(0,0), true, White, Indigo);
+    EADK::Display::drawString("However there is no active scene.", EADK::Point(0, 20), false, White, Indigo);
 
     int running = 1;
 
